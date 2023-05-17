a354ddb7287f4f1702ae7f00ab4cb95fd14a3c18
Anixias <colnusca@gmail.com>
2023-05-17 11:53:47 -0400

    Created SemanticAnalyzer

---
+++ GuildScript/Analysis/Semantics/SemanticAnalyzer.cs
@@ -0,0 +1,269 @@
+namespace GuildScript.Analysis.Semantics;
+
+public sealed class SemanticAnalyzer : ResolvedStatement.IVisitor, ResolvedExpression.IVisitor
+{
+	public void VisitProgramStatement(ResolvedStatement.Program statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitEntryPointStatement(ResolvedStatement.EntryPoint statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitDefineStatement(ResolvedStatement.Define statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitBlockStatement(ResolvedStatement.Block statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitClassStatement(ResolvedStatement.Class statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitStructStatement(ResolvedStatement.Struct statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitInterfaceStatement(ResolvedStatement.Interface statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitEnumStatement(ResolvedStatement.Enum statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitDestructorStatement(ResolvedStatement.Destructor statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitExternalMethodStatement(ResolvedStatement.ExternalMethod statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitConstructorStatement(ResolvedStatement.Constructor statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitIndexerStatement(ResolvedStatement.Indexer statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitAccessorAutoStatement(ResolvedStatement.AccessorAuto statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitAccessorLambdaStatement(ResolvedStatement.AccessorLambda statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitAccessorLambdaSignatureStatement(ResolvedStatement.AccessorLambdaSignature statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitEventStatement(ResolvedStatement.Event statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitEventSignatureStatement(ResolvedStatement.EventSignature statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitPropertyStatement(ResolvedStatement.Property statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitPropertySignatureStatement(ResolvedStatement.PropertySignature statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitMethodStatement(ResolvedStatement.Method statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitMethodSignatureStatement(ResolvedStatement.MethodSignature statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitFieldStatement(ResolvedStatement.Field statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitBreakStatement(ResolvedStatement.Break statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitContinueStatement(ResolvedStatement.Continue statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitControlStatement(ResolvedStatement.Control statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitWhileStatement(ResolvedStatement.While statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitDoWhileStatement(ResolvedStatement.DoWhile statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitForStatement(ResolvedStatement.For statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitForEachStatement(ResolvedStatement.ForEach statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitRepeatStatement(ResolvedStatement.Repeat statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitReturnStatement(ResolvedStatement.Return statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitThrowStatement(ResolvedStatement.Throw statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitSealStatement(ResolvedStatement.Seal statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitTryStatement(ResolvedStatement.Try statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitVariableDeclarationStatement(ResolvedStatement.VariableDeclaration statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitLockStatement(ResolvedStatement.Lock statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitSwitchStatement(ResolvedStatement.Switch statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitExpressionStatement(ResolvedStatement.ExpressionStatement statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitOperatorOverloadStatement(ResolvedStatement.OperatorOverload statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitOperatorOverloadSignatureStatement(ResolvedStatement.OperatorOverloadSignature statement)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitAwaitExpression(ResolvedExpression.Await expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitConditionalExpression(ResolvedExpression.Conditional expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitBinaryExpression(ResolvedExpression.Binary expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitTypeRelationExpression(ResolvedExpression.TypeRelation expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitUnaryExpression(ResolvedExpression.Unary expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitIdentifierExpression(ResolvedExpression.Identifier expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitQualifierExpression(ResolvedExpression.Qualifier expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitCallExpression(ResolvedExpression.Call expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitLiteralExpression(ResolvedExpression.Literal expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitInstantiateExpression(ResolvedExpression.Instantiate expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitCastExpression(ResolvedExpression.Cast expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitIndexExpression(ResolvedExpression.Index expression)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void VisitLambdaExpression(ResolvedExpression.Lambda expression)
+	{
+		throw new NotImplementedException();
+	}
+}
